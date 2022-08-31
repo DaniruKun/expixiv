cf8b887a741b3f59012667cdfa01507a283ae1e3
Daniils Petrovs <thedanpetrov@gmail.com>
2022-09-01 01:04:38 +0200

    Initial client prototype

--- lib/client.ex
+++ lib/client.ex
@@ -2,7 +2,6 @@ defmodule Expixiv.Client do
   @moduledoc """
   Custom Tesla-based Pixiv API HTTP client.
   """
-  use Tesla
 
   @headers [
     {"User-Agent", "PixivIOSApp/7.6.2 (iOS 12.2; iPhone9,1)"},
@@ -11,7 +10,23 @@ defmodule Expixiv.Client do
     {"App-OS", "ios"}
   ]
 
-  plug(Tesla.Middleware.BaseUrl, "https://app-api.pixiv.net")
-  plug(Tesla.Middleware.Headers, @headers)
-  plug(Tesla.Middleware.JSON, engine: Poison, engine_opts: [keys: :atoms])
+  @base_url "https://app-api.pixiv.net"
+
+  @spec client(String.t()) :: Tesla.Client.t()
+  def client(access_token) do
+    middleware = [
+      {Tesla.Middleware.BaseUrl, @base_url},
+      {Tesla.Middleware.Headers, @headers},
+      {Tesla.Middleware.BearerAuth, token: access_token},
+      {Tesla.Middleware.JSON, engine: Poison, engine_opts: [keys: :atoms]}
+    ]
+
+    Tesla.client(middleware)
+  end
+
+  @spec user_details(Tesla.Client.t(), any) :: {:error, any} | {:ok, Tesla.Env.t()}
+  def user_details(client, user_id) do
+    path = "/v1/user/detail"
+    Tesla.get(client, path, query: [user_id: user_id, filter: "for_ios"])
+  end
 end
